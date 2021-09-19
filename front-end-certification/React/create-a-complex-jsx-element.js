800c80d9a797cb8d8dd1405b75a177647896722b
Felipe Belisário <felipesantos.dsb@hotmail.com>
2021-09-18 21:52:39 -0300

    React files added

---
+++ front-end-certification/React/create-a-complex-jsx-element.js
@@ -0,0 +1,8 @@
+const JSX = <div><h1>Hello</h1>
+<p>World</p>
+<ul>
+<li>Free</li>
+<li>Code</li>
+<li>Camp</li>
+</ul>
+</div>
---
+++ front-end-certification/React/create-a-stateless-functional-component.js
@@ -0,0 +1,9 @@
+const MyComponent = function() {
+  // Change code below this line
+return (
+  <div> Meu Texto </div>
+)
+
+
+  // Change code above this line
+}
